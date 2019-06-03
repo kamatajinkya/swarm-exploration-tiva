5a5eaa8b64b426b392dc6e00b9a67003ab77688e
Ajinkya Kamat <kamat.ajinkya@gmail.com>
2019-06-03 19:42:33 -0400

    Commiting forgotten device file

---
+++ modules/device/device.h
@@ -0,0 +1,17 @@
+/*
+ * device.h
+ *
+ *  Created on: May 25, 2019
+ *      Author: kamat
+ */
+
+#ifndef MODULES_
+#define HARDWARE_DEVICE_DEVICE_H_
+
+/// Initializes the device. ie sets up the correct clock.
+/// Enables and configures the peripherals etc
+void device_init();
+
+
+
+#endif /* HARDWARE_DEVICE_DEVICE_H_ */
